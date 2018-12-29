0496850cdadadcd8934c01d4681ac71b32c26e31
skas <skas@live.co.uk>
2018-12-29 12:46:55 +0300

    Adding Car Animation

---
+++ scripts/Galactic_Car.gd
@@ -0,0 +1,26 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+func play(anim):
+	match anim:
+		"left":
+			$AnimatedSprite.animation = "right"
+			$AnimatedSprite.transform.scaled(Vector2(-1,1))
+			self.rotation = 0
+			print("yay i eez animating right dude")
+		"top":
+			$AnimatedSprite.animation = "forward"
+			$AnimatedSprite.transform.scaled(Vector2(1,1))
+			print("yay i eez animating forward dude")		
